5cb4db80b8a5eb5cc9569e183b10815d6d6f1654
Tanja <tanjamilisavljevic7@gmail.com>
2021-06-25 12:42:17 +0200

    Add basic javascript and html

---
+++ script.js
@@ -0,0 +1,20 @@
+
+document.getElementById('drawButton').addEventListener('click', function () {
+
+    function getRandomCard(max) {
+        return Math.floor(Math.random() * max + 1);
+    }
+
+    let card1 = getRandomCard(14);
+    let card2 = getRandomCard(14);
+
+    document.getElementById("card1").textContent = '' + card1;
+    document.getElementById("card2").textContent = '' + card2;
+
+    if (card1.value + card2.value === 21){
+        alert("You win")
+    }
+    else {
+        alert("You lose")
+    }
+})
