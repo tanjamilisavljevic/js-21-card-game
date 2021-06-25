cd1a8c675ef466e82ba2b6cba01c919a7b6cb317
Tanja <tanjamilisavljevic7@gmail.com>
2021-06-25 15:31:03 +0200

    Add random symbols to cards

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const cardSymbols =['♠', '♣', '♥', '♦']
+function getRandomSymbol() {
+    let randomIndex = Math.floor(Math.random() * cardSymbols.length);
+    return cardSymbols[randomIndex]}
 
 
 function addCardStyling(element) {
@@ -10,17 +14,19 @@ document.getElementById('drawButton').addEventListener('click', function () {
         return Math.floor(Math.random() * max + 1);
     }
 
-    let card1 = getRandomCard(14);
-    let card2 = getRandomCard(14);
+    let card1text = getRandomCard(14);
+    let card2text = getRandomCard(14);
 
-    document.getElementById("card1").textContent = '' + card1;
-    document.getElementById("card2").textContent = '' + card2;
+    document.getElementById("card1text").textContent = '' + card1text;
+    document.getElementById("randomSymbol1").innerHTML = getRandomSymbol();
+    document.getElementById("card2text").textContent = '' + card2text;
+    document.getElementById("randomSymbol2").innerHTML = getRandomSymbol();
 
     addCardStyling('card1');
     addCardStyling('card2');
 
 
-    if (card1.value + card2.value === 21) {
+    if (card1text.value + card2text.value === 21) {
         alert("You win")
     } else {
         alert("You lose")
